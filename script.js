f3c8b59cb83d3aae1339e79826c160b1a9fd3878
Sunil Preethik <sunilpreethik@sunils-air.speedport.ip>
2020-10-28 11:46:43 +0100

    Description about sending real emails

--- script.js
+++ script.js
@@ -11,12 +11,6 @@ const db = knex({
   }
 });
 
-db('users').count('email')
-  .then (count => {
-  getUserEmail(count[0].count);
-  console.log(count[0].count);
-});
-
 let transporter = nodemailer.createTransport({
   host: "smtp.mailtrap.io",
   port: 2525,
@@ -33,13 +27,28 @@ let mail = {
   text: 'Hello There!!!'
 };
 
+db('users').count('email')
+  .then (count => {
+  getUserEmail(count[0].count);
+  console.log(count[0].count);
+});
+
+//getUserEmail(10);
+
 function getUserEmail(count){
   for(let i = 0; i < count; i++){
     db.table('users').pluck('email')
     .then( user => {
       mail.to = user[i];
       console.log(mail.to)
-      //sendMail(mail)
+
+      //The smtp service provider which is being used allows only 5 emails per second and maximum 
+      //of 500 emails per account for the free version.
+
+      //Uncomment line 51 && 36 and comment line 32 to send real emails. 
+      //The count value is changed to small number to meet the limitations set by the smtp service provider 
+
+      //sendMail(mail) 
     })
   }
 }
